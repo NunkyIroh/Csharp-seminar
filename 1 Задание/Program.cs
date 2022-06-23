214695401a1b5446ce77340cef99b1d7eaa955ce
Uncle_Iroh <vadegtyarev@gmail.com>
2022-06-23 23:24:35 +0500

    Задание 1

---
+++ 1 Задание/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+//Console.Write("Введите целое число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//double square = Math.Pow(num, 2);
+
+double d = 5.55;
+int i = 2022;
+byte b = 155;
+b = (byte)i;
+Console.Write(b);
+//Console.Write($"Квадрат числа {num} будет равен {square} ");
