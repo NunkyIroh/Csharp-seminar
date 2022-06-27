85a9fbe03ff3cbab951f6411d65c62f5fd0551f5
Uncle_Iroh <vadegtyarev@gmail.com>
2022-06-27 23:10:36 +0500

    Seminar_2_Task_1/

---
+++ Seminar_2_Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int rndNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {rndNum}=>{maxDigit}");
+
+// int maxDigit = firstDigit;
+// if (secondDigit > maxDigit) maxDigit = secondDigit;
+// else if (secondDigit == maxDigit) maxDigit = 0;
+
+// if (maxDigit > 0) Console.WriteLine($"Наибольшая цифра числа {rndNum}=>{maxDigit}");
+// else Console.WriteLine($"Цифры числа {rndNum} равны");
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int maxDigit = MaxDigit(rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+Console.WriteLine($"Наибольшая цифра числа {rndNum}=>{result}");
