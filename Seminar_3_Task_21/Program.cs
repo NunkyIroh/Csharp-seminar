71613c9aa753572de7dc4f26f3f5ea389c46d7ac
Uncle_Iroh <vadegtyarev@gmail.com>
2022-06-30 23:48:04 +0500

    Seminar_3_Task_21

---
+++ Seminar_3_Task_21/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки А: ");
+Console.Write("xA: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("yA: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки В: ");
+Console.Write("xB: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("yB: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow((xA - xB), 2) + Math.Pow((yA - yB), 2));
+Console.WriteLine(Math.Round(AB, 2));
