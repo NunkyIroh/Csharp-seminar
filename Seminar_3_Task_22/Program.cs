86167fec936d905b7ca2d026a9102aa4be51c977
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-01 22:54:31 +0500

    Seminar_3_Task_22

---
+++ Seminar_3_Task_22/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+int result = 0;
+
+while (index <= N)
+{
+    Console.Write(index);
+    result = index * index;
+    index++;
+    Console.WriteLine($" {result,4}");
+}
+
+
