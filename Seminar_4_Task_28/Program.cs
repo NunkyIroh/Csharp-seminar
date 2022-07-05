be3d87baa2662605d993d2b2d1fd97bb8993f538
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-05 22:33:17 +0500

    Seminar_4_Task_28

---
+++ Seminar_4_Task_28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+
+int Sum (int number)
+{
+
+    int sum = 1;
+    for (int i = 1; i <= number; i++)
+    {
+    sum = sum * i;
+    }
+    return sum;
+
+}
+
+int result = Sum(num);
+Console.WriteLine($"Произведение чисел от 1 до N: {result}");
