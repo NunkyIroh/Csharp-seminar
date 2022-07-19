d291c9df7053be57c0371a93b92f07699565906e
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-19 09:21:42 +0300

    Двумерный массив. Программа, поменяет местами первую и последнюю строку массива.

---
+++ Seminar_8_Task_53/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void ExchangeMatrix(int[,] array)
+{
+    int temp = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        temp = array[0, j];
+        array[0, j] = array[array.GetLength(0) - 1, j];
+        array[array.GetLength(0) - 1, j] = temp;
+    }
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3}| ");
+            else Console.Write($"{arr[i, j],3} |");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arrayResult = CreateMatrixRndInt(3, 4, 1, 20);
+PrintMatrix(arrayResult);
+Console.WriteLine();
+ExchangeMatrix(arrayResult);
+PrintMatrix(arrayResult);
