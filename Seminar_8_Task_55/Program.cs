ce57838263ad2b75340db09e0207e011673c030c
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-19 09:23:42 +0300

    двумерный массив. Программа, заменяет строки на столбцы

---
+++ Seminar_8_Task_55/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+Console.Clear();
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] arr = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+int[,] ExchangeMatrix(int[,] array)
+{
+    int[,] newArray = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[i, j] = array[j, i];
+        }
+    }
+    return newArray;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("|");
+            if (j < arr.GetLength(1) - 1) Console.Write($"{arr[i, j],3}| ");
+            else Console.Write($"{arr[i, j],3} |");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] arrayResult = CreateMatrixRndInt(4, 4, 1, 20);
+PrintMatrix(arrayResult);
+Console.WriteLine();
+if (arrayResult.GetLength(0) != arrayResult.GetLength(1)) Console.WriteLine("Невозможно произвести замену");
+else
+{
+    int[,] arrResult = ExchangeMatrix(arrayResult);
+    PrintMatrix(arrResult);
+}
