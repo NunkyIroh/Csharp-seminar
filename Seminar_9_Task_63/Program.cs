95591aaabba8f5a3d6cf42f005b9ec76bb15ddac
Uncle_Iroh <vadegtyarev@gmail.com>
2022-07-22 18:38:48 +0300

    Программа принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.

---
+++ Seminar_9_Task_63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Clear();
+Console.WriteLine("Задайте число: ");
+int num = int.Parse(Console.ReadLine());
+
+void NaturalNumber (int number)
+{
+    if(number == 0) return;
+    // Console.Write($"{number} "); если надо запасать наоборот
+    NaturalNumber(number - 1);
+    Console.Write($"{number} ");
+}
+
+NaturalNumber(num);
